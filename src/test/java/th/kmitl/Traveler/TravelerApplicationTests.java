3a9c8c4cfdd6323985e78654876a2565678635df
patcharin063 <61050893@kmitl.ac.th>
2020-11-16 02:04:00 +0700

    first commit

---
+++ src/test/java/th/kmitl/Traveler/TravelerApplicationTests.java
@@ -0,0 +1,13 @@
+package th.kmitl.Traveler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
